9964538400c1935dd88f7c5f5b76306763230b32
Kristian Kanchev <kristianfzr@gmail.com>
2020-01-19 14:15:01 +0200

    Add project files.

---
+++ WebSiteProject-46033r/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteProject_46033r.Startup))]
+namespace WebSiteProject_46033r
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
